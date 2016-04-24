1461ec8f8fba690ab413d2b24c509f2d6611d2b9
rdingen12 <helsdin2@illinois.edu>
2016-04-24 15:12:11 -0500

    Game_Logic updated

--- Lab_Code/Final_Frogger/game_logic.sv
+++ Lab_Code/Final_Frogger/game_logic.sv
@@ -5,121 +5,85 @@
 //-------------------------------------------------------------------------
 
 
-module  game_logic ( input game_restart, frame_clk,
-               output [3:0]  tens_digit, ones_digit		//10 possible digits
-					
+module  game_logic ( input game_restart, 						//push button that signals restart the game
+									frame_clk, 
+									isDead,								//is the Frog dead?
+									isAlive,								//is the Frog safely on the other side?
+               output [3:0]  tens_digit, ones_digit,		//10 possible digits
+									
 				 );
 				 
-				 
+	
+	logic [5:0] game_clock;
+	logic [1:0] frog_wins;
+	parameter [5:0] clock_restart = 6'd60;
+	parameter [1:0] frog_wins_restart = 2'd0;
+	parameter [1:0] frog_wins_gameover = 2'd3;
+
 				 
 	always_comb 
 	begin
       next_state = state;
       case (state)
-			RESET: next_state = CLOCK_30;
+			if (game_restart == 1'd1) next_state = START;
 			
-			CLOCK_30: begin
-							if (game_restart) next_state = RESET;
-							else next_state = CLOCK_29;
+			RESTART_OR_NAH: //checks if player wishes to start a new game
+					begin
+						next_state = RESTART_OR_NAH;
+						//will restart if game_restart is true
 			end
 			
-			CLOCK_29: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_28;
+			START: begin
+						game_clock <= clock_restart;			//reset clock
+						frog_wins <= frog_wins_restart;		//reset wins
+						next_state <= CONTINUE;	
+						//reset isDead
 			end
 			
-			CLOCK_28: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_27;
+			CONTINUE: 
+						next_state = WAIT;
+			//reset frog position, isAlive
+			
+			WAIT: begin
+						if (/*something with clock every second*/)
+							next_state = INCREMENT_CLK;
+						else if (isDead == 1'd1)
+							next_state = DEAD;
+						else if (isAlive == 1'd1)
+							next_state = WIN;
+						else
+							next_state = WAIT;
 			end
 			
-			CLOCK_27: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_26;
+			//deduct a second from the game clock
+			INCREMENT_CLK: begin
+						game_clock <= game_clock - 1'd1;
+						if (game_clock = 1'd0)
+							next_state = DEAD;
+						else
+							next_state = WAIT;
+			end
+
+			
+			DEAD: begin
+						next_state <= RESTART_OR_NAH;
 			end
 			
-			CLOCK_26: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_25;
+			WIN: begin
+						if (frog_wins == frog_wins_gameover)
+								next_state = RESTART_OR_NAH;
+						else 
+								next_state = CONTINUE;
 			end
 			
-			CLOCK_25: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_;
-			end
 			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			CLOCK_30: begin
-							if (game_reset) next_state = RESET;
-							else next_state = CLOCK_29;
-			end
-			
-			WAIT: next_state = (game_reset) ? MOVE : WAIT;
-			GAMEOVER
-			
-			MOVE:	next_state = WAIT;
 		endcase
+	end
+	
+	//determining tens_digit and ones_digit
+	always_comb 
+	begin
+			ones_digit = game_clock % 4'd10;
+			tens_digit = game_clock / 4'd10;		
+	end
+	
